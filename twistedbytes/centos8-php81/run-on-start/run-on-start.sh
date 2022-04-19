238358ca0e871090ddf6877d4afc18dedc487ca0
Derk Gortemaker <derk@gortemaker.nl>
2022-04-19 11:49:01 +0200

    make php81 usefull on buddy, do nothing with startup when no command and no _TB_ vars set

--- twistedbytes/centos8-php81/run-on-start/run-on-start.sh
+++ twistedbytes/centos8-php81/run-on-start/run-on-start.sh
@@ -1,49 +1,51 @@
 #!/bin/bash
 set -e
-# set -x
+set -x
 
 _TB_START_CMD="$@"
 
 _TB_LAST_COMMAND="/bin/bash"
 
 # if No command and No _TB_ vars do no run any of the startup commands.
-if [[ -n $_TB_START_CMD ]] || [[ $( export | grep _TB_ | grep -v -e '_TB_TZ' | wc -l ) -ge 1 ]]; then
+if [[ -z $_TB_START_CMD ]] && [[ $( export | grep _TB_ | grep -v -e '_TB_TZ' | wc -l ) -eq 0 ]]; then
+  exec /bin/bash
+  exit 0
+fi
 
 export _TB_START_CMD
 
-  while [[ $# -gt 0 ]]; do
-    key="$1"
+while [[ $# -gt 0 ]]; do
+  key="$1"
 
-    case $key in
-        debug)
-            export _TB_DEBUG="-x"
-            set ${_TB_DEBUG}
-        ;;
-        phpfpm)
-            _TB_LAST_COMMAND="exec gosu www-data php-fpm -F -O"
-            if [[ ${_TB_PHPFPM} != Y ]]; then
-              echo "make sure to set the _TB_PHPFPM env variable to Y"
-              exit 0
-            fi
-        ;;
-
-        help)
-            grep -h -A1  -e '^# INPUTVAR' /aa-run-on-start/scripts/*.sh | sed -e 's/^#//g'
+  case $key in
+      debug)
+          export _TB_DEBUG="-x"
+          set ${_TB_DEBUG}
+      ;;
+      phpfpm)
+          _TB_LAST_COMMAND="exec gosu www-data php-fpm -F -O"
+          if [[ ${_TB_PHPFPM} != Y ]]; then
+            echo "make sure to set the _TB_PHPFPM env variable to Y"
             exit 0
-        ;;
-        *)
-        ;;
-    esac
-    shift # past argument or value
-  done
+          fi
+      ;;
 
-  if [[ -d /aa-run-on-start/scripts ]]; then
-    for i in /aa-run-on-start/scripts/*.sh; do
-      [[ -n $_TB_DEBUG ]] && echo running $i;
-      bash -e ${_TB_DEBUG} $i
-      # sleep 0.1
-    done
-  fi
+      help)
+          grep -h -A1  -e '^# INPUTVAR' /aa-run-on-start/scripts/*.sh | sed -e 's/^#//g'
+          exit 0
+      ;;
+      *)
+      ;;
+  esac
+  shift # past argument or value
+done
+
+if [[ -d /aa-run-on-start/scripts ]]; then
+  for i in /aa-run-on-start/scripts/*.sh; do
+    [[ -n $_TB_DEBUG ]] && echo running $i;
+    bash -e ${_TB_DEBUG} $i
+    # sleep 0.1
+  done
 fi
 
 ${_TB_LAST_COMMAND}
