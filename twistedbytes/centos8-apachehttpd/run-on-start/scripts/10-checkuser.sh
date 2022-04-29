ba85dfe7641e28e45139780a8f642230950a9a98
Derk Gortemaker <derk@gortemaker.nl>
2022-04-29 13:26:09 +0200

    Update apachehttpd to handle other uid than 1000 change setupproject to use the change

--- twistedbytes/centos8-apachehttpd/run-on-start/scripts/10-checkuser.sh
+++ twistedbytes/centos8-apachehttpd/run-on-start/scripts/10-checkuser.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 #-- help section
-# INPUTVAR _TB_PHPFPM
-#     Only value is Y, if Y then php-fpm config is created
 
 #-- end help section, keep 1 line free above
 
-if [[ ${_TB_PHPFPM} != Y ]]; then
-  exit 0
-fi
-
 if [[ $( id -u ) -ne 0 ]]; then
+  echo "detect userid: $( id -u )"
   echo "please do no use 'user: ' in docker composer for this"
   echo "this docker will switch to the right user itself when using the right ENV vars"
   echo "Use ENV _TB_UIDGID_FROMDIR or _TB_UIDGID for that"
---
+++ twistedbytes/centos8-apachehttpd/run-on-start/scripts/21-fix-path-permissions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# set -x
+
+chown -Rf www-data:www-data \
+    /var/log/httpd /run/httpd
