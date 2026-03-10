903674bc56ed4af2da30d712ecad7e3e67a1252a
Derk Gortemaker <derk@twistedbytes.nl>
2026-03-10 15:41:17 +0100

    add php52/3 images

---
+++ twistedbytes/twistedbytes-apache-php52-docker/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+docker build --rm -t "twistedbytes/apache-php52:20221205" -t "twistedbytes/apache-php52:latest" .
---
+++ twistedbytes/twistedbytes-apache-php53-docker/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+# docker build --rm -t "twistedbytes/apache-php52:20221205" -t "twistedbytes/apache-php52:latest" .
+
+podman build --rm -t "twistedbytes/apache-php53:20260305" -t "twistedbytes/apache-php53:latest" .
+
+podman save "twistedbytes/apache-php53:latest" | pv | ssh yview.dev2@csl-yview-dev-01.cslweb.uk -p2223 podman load
