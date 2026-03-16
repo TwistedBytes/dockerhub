e57f968010d3b36055b54c69527b9be1db9a3a9d
Derk Gortemaker <derk@gortemaker.nl>
2026-03-16 16:31:53 +0100

    add msmtp

--- twistedbytes/twistedbytes-apache-php53-docker/build.sh
+++ twistedbytes/twistedbytes-apache-php53-docker/build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+set -e
 
 # docker build --rm -t "twistedbytes/apache-php52:20221205" -t "twistedbytes/apache-php52:latest" .
 
-podman build --rm -t "twistedbytes/apache-php53:20260305" -t "twistedbytes/apache-php53:latest" .
+podman build --rm -t "twistedbytes/apache-php53:$(date +%Y%m%d)" -t "twistedbytes/apache-php53:latest" .
 
 podman save "twistedbytes/apache-php53:latest" | pv | ssh yview.dev2@csl-yview-dev-01.cslweb.uk -p2223 podman load
